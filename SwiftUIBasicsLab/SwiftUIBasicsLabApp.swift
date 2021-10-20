0802411e3233c7501a659df212261dc16a49d58b
jreyes72 <jreyes72@depaul.edu>
2021-10-20 14:50:00 -0500

    Initial Commit

---
+++ SwiftUIBasicsLab/SwiftUIBasicsLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicsLabApp.swift
+//  SwiftUIBasicsLab
+//
+//  Created by Central States SER mac  on 10/20/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicsLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
